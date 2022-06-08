1ed11593c172e8ddbb741b59749b123d7152b110
17043003 <ishizuka533725@gmail.com>
2022-06-08 21:43:04 +0900

    first commit.

---
+++ app/src/main/java/com/ishzk/android/newspicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ishzk.android.newspicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
