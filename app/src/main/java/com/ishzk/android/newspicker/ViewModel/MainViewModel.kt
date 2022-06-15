7218768d97966d6616edb94f3c63b61cec970c3e
17043003 <ishizuka533725@gmail.com>
2022-06-16 03:06:08 +0900

    change using livedata about NewsList at MainViewModel.

--- app/src/main/java/com/ishzk/android/newspicker/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/ishzk/android/newspicker/ViewModel/MainViewModel.kt
@@ -1,26 +1,25 @@
 package com.ishzk.android.newspicker.ViewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import com.ishzk.android.newspicker.BuildConfig
 import com.ishzk.android.newspicker.Model.Article
 import com.ishzk.android.newspicker.Service.NewsApiService
 import com.ishzk.android.newspicker.Service.RetrofitClient
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainViewModel: ViewModel() {
-    var service: NewsApiService = RetrofitClient.getService()
-    lateinit var newsList: List<Article>
+    private var service: NewsApiService = RetrofitClient.getService()
+    val newsList: MutableLiveData<List<Article>> by lazy { MutableLiveData<List<Article>>() }
 
     fun fetchNews(query: String, fromDate: Date){
-        CoroutineScope(Dispatchers.IO).launch {
-            val from = SimpleDateFormat("yyyy-MM-dd").format(fromDate)
-            val result = service.getNews(query, BuildConfig.apikey, from).execute().body()
-            if(result != null && result.status == "ok"){
-                newsList = result.articles
+        liveData<List<Article>> {
+            val from = SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(fromDate)
+            val result = service.getNews(query, BuildConfig.apikey, from).execute().body() ?: return@liveData
+            if(result.status == "ok"){
+                newsList.value = result.articles
             }
         }
     }
