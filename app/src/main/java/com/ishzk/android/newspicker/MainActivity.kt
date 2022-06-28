5934167350853452390cd41fc1f2be7162ff896c
17043003 <ishizuka533725@gmail.com>
2022-06-29 06:00:21 +0900

    fetch news from news api.
    finish fetching news, livedata is posted.
    news is shown by view binding in recyclerview.

--- app/src/main/java/com/ishzk/android/newspicker/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/newspicker/MainActivity.kt
@@ -2,10 +2,81 @@ package com.ishzk.android.newspicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.*
+import com.ishzk.android.newspicker.databinding.ActivityMainBinding
+import com.ishzk.android.newspicker.databinding.NewsRowBinding
+import com.ishzk.android.newspicker.model.Article
+import com.ishzk.android.newspicker.viewmodel.MainViewModel
+import java.util.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private val newsListAdapter: NewsListAdapter by lazy { NewsListAdapter(viewModel) }
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchView.setOnQueryTextListener(NewsSearchViewListener())
+
+        binding.newsView.adapter = newsListAdapter
+        binding.newsView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        viewModel.newsList.observe(this){
+            newsListAdapter.submitList(it)
+        }
+    }
+
+    inner class NewsSearchViewListener: SearchView.OnQueryTextListener{
+        override fun onQueryTextChange(newText: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            viewModel.fetchNews(query ?: "", Date())
+            return true
+        }
+    }
+
+    private object DiffCallback : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private inner class NewsListAdapter(private val viewModel: MainViewModel):
+        ListAdapter<Article, NewsListViewHolder>(DiffCallback){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+
+            return NewsListViewHolder(NewsRowBinding.inflate(layoutInflater, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
+            holder.bind(getItem(position), viewModel)
+        }
+    }
+
+    private inner class NewsListViewHolder(private val binding: NewsRowBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(item: Article, viewModel: MainViewModel) {
+            binding.run {
+                binding.article = item
+                executePendingBindings()
+            }
+        }
     }
 }
--- app/src/main/java/com/ishzk/android/newspicker/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/ishzk/android/newspicker/ViewModel/MainViewModel.kt
@@ -1,25 +1,36 @@
 package com.ishzk.android.newspicker.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.ishzk.android.newspicker.BuildConfig
 import com.ishzk.android.newspicker.model.Article
 import com.ishzk.android.newspicker.service.NewsApiService
 import com.ishzk.android.newspicker.service.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
+
+private const val TAG = "MainViewModel"
 
 class MainViewModel: ViewModel() {
     private var service: NewsApiService = RetrofitClient.getService()
     val newsList: MutableLiveData<List<Article>> by lazy { MutableLiveData<List<Article>>() }
 
     fun fetchNews(query: String, fromDate: Date){
-        liveData<List<Article>> {
-            val from = SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(fromDate)
-            val result = service.getNews(query, BuildConfig.apikey, from).execute().body() ?: return@liveData
-            if(result.status == "ok"){
-                newsList.value = result.articles
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val from = SimpleDateFormat("yyyy-MM-dd", Locale.JAPAN).format(fromDate)
+                val result = service.getNews(query, BuildConfig.apikey, from).execute().body()
+                    ?: return@withContext
+                if (result.status == "ok") {
+                    newsList.postValue(result.articles)
+                    Log.v(TAG, "total articles:${result.totalResults}")
+                }
             }
         }
     }
